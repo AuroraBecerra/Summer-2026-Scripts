4594a52a25debdcd551db17faa47a4633b81915d
AuroraBecerra <aurora.ep.2357@gmail.com>
2026-01-08 00:39:26 -0300

    Hemos creado el primer archivo: la carpeta de los scripts junto con el primer script creado (vacío). En este último escribiremos el primer intento para resolver las ecuaciones (2) del paper con el método Leapfrog (salto de rana).

---
+++ Scripts/LF-1st-Attempt.py
@@ -0,0 +1,2 @@
+# Este es el primer script de Python (.py) creado para inicializar el parendizaje de enero 2026 mediante el paper PEFTCOTHMFM.
+# En este script tenemos como tarea escribir y ejecutar un código acorde con el algoritmo Leapfrog para resolver las ecuaciones de movimiento (ecuaciones diferenciales) (2) mostradas en el paper.
